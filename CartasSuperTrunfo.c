ea7c28def1edef7182376070dea3beaa7b98d323
NegoDrama830 <sgf11035@gmail.com>
2025-03-21 13:06:21 +0000

    Adicionando o calculo da densidade populacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,7 @@ int main(){
     int populacao1, populacao2, NPT1, NPT2;
     float area1, area2, PIB1, PIB2;
     char nome1[50], nome2[50];
+    float densidade1, densidade2, pibpercapita2, pibpercapita1;
 
 //Dando ínicio ao sistema da 1° carta pedindo informaçãoes da cidade.
     printf("-----PRIMEIRA CARTA------ \n");
@@ -26,17 +27,23 @@ int main(){
 
 //Informe o PIB(Produto Interno Bruto da cidade)    
     printf("Digite o PIB da sua cidade: \n");
-    scanf("%f",&PIB1);
+    scanf("%.2f",&PIB1);
 
 //Informe a quantidade de pontos turisticos da sua cidade
     printf("Há quantos pontos turisticos na sua cidade: \n");
     scanf("%d", &NPT1);
     printf("\n");
 
-//Analisando e colocando as informações da 1° da carta descritas pelo úsuario na tela
+//Calculando a densidade populacional e o PIB per capita da primeira carta
+    densidade1 = populacao1 / area1;
+    pibpercapita1 = PIB1 / populacao1;
+
+//Analisando e colocando as informações da 1° da carta descritas pelo úsuario na tela junto aos resultados do calcúlo
     printf("Cidade: %s\n População: %d\n", nome1, populacao1);
     printf("Área: %f Km² \n  PIB: %f bilhões de reais \n", area1, PIB1);
     printf("Número de Pontos turisticos: %d\n", NPT1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per capita: %.2f reais\n", pibpercapita1);
     printf("\n");
     printf("\n");
 
@@ -55,17 +62,23 @@ int main(){
 
 //Informe o PIB(Produto Interno Bruto da cidade)  
     printf("Digite o PIB da sua cidade: \n");
-    scanf("%f",&PIB2);
+    scanf("%.2f",&PIB2);
 
 //Informe a quantidade de pontos turisticos da sua cidade
     printf("Há quantos pontos turisticos na sua cidade: \n");
     scanf("%d", &NPT2);
     printf("\n");
 
-//Analisando e colocando as informações da 2° da carta descritas pelo úsuario na tela
+//Calculando a densidade populacional e o PIB per capita da segunda carta
+    densidade2 = populacao2 / area2;
+    pibpercapita2 = PIB2 / populacao2;
+
+//Analisando e colocando as informações da 2° da carta descritas pelo úsuario na tela junto aos resultados do calcúlo
     printf("Cidade: %s\n População: %d\n", nome2, populacao2);
     printf("Área: %f Km² \n  PIB: %f bilhões de reais \n ", area1, PIB2);
-    printf("Número de Pontos turisticos: %d", NPT2);
+    printf("Número de Pontos turisticos: %d\n", NPT2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per capita: %.2f reais\n", pibpercapita2);
 
 //Encerrando programa Desafio Super Trunfo - Países
     return 0;
